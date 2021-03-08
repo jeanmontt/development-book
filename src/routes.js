9b893510e0a702b2eafce5bfccca5649b420a35e
Jean Monteiro <jeanmontt@hotmail.com>
2021-03-08 19:40:30 +0000

    Update

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,17 @@ const express = require('express');
 
 const routes = express.Router();
 
+//INDEX
 routes.get("/", (req, res) => {
   return res.render("index");
+});
+
+//HTML
+routes.get("/html", (req, res) => {
+  return res.render("techs/html/00_index");
+});
+routes.get("/html/conteudo", (req, res) => {
+  return res.render("techs/html/01_subjects-layout");
 });
 
 module.exports = routes;
